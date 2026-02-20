3d8da2196180e07c286eb13097392506e9a9d53b
mayamulikqa@gmail.com <mayavmulik@gmail.com>
2026-02-20 19:20:42 +0530

    updated code

---
+++ src/test/java/utility/ExcelUtils.java
@@ -0,0 +1,24 @@
+package utility;
+
+import org.apache.poi.xssf.usermodel.*;
+import java.io.FileInputStream;
+
+public class ExcelUtils {
+    public static Object[][] getExcelData(String filePath, String sheetName) throws Exception {
+        FileInputStream fis = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet(sheetName);
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int colCount = sheet.getRow(0).getLastCellNum();
+        
+        Object[][] data = new Object[rowCount - 1][colCount];
+        for (int i = 1; i < rowCount; i++) {
+            XSSFRow row = sheet.getRow(i);
+            for (int j = 0; j < colCount; j++) {
+                data[i - 1][j] = row.getCell(j).toString();
+            }
+        }
+        workbook.close();
+        return data;
+    }
+}
